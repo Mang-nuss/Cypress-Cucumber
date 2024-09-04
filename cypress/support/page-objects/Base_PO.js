4222b8a053e7a369efe0e993e070c3f879316d69
Magnus Johansson <59422395+Mang-nuss@users.noreply.github.com>
2024-09-04 11:29:44 +0200

    needed to comment out the reference types line

--- cypress/support/page-objects/Base_PO.js
+++ cypress/support/page-objects/Base_PO.js
@@ -1,11 +1,15 @@
-/ <reference types="cypress" />
+/// <reference types="cypress" />
 
 class Base_PO {
     //baseUrl = "https://www.randstad.se";
 
     navigate(path) {
-        cy.visit(baseUrl + path);
+        //cy.visit(baseUrl + path);
+        cy.fixture("config.json").then((data) => {
+
+            cy.visit(data.baseUrl + path)
+        })
     }
 }
 
-export default
+export default Base_PO;
